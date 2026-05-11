b8802abb69bc6eabaa2f6ee097c7838fdce0cfa2
lhb986666 <gksqls5936@gmail.com>
2026-05-11 10:37:03 +0900

    fix: API URL EC2로 고정

--- mobile/src/api/client.js
+++ mobile/src/api/client.js
@@ -1,9 +1,7 @@
 import axios from 'axios'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
-const BASE_URL = __DEV__
-  ? 'http://192.168.2.35:8080'
-  : 'http://54.116.69.43:8080'
+const BASE_URL = 'http://54.116.69.43:8080'
 
 const client = axios.create({ baseURL: BASE_URL })
 
